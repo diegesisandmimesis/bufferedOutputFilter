0e75ce57e3106a3ee496288566f6f624a3a9ed41
Diegesis & Mimesis <diegesisandmimesis@proton.me>
2025-10-15 21:12:23 -0700

    Added header guard

--- bufferedOutputFilter.h
+++ bufferedOutputFilter.h
@@ -4,3 +4,6 @@
 
 // Uncomment to enable debugging options.
 //#define __DEBUG_BUFFERED_OUTPUT_FILTER
+
+// Do not comment this out.  It's for dependency checking in other modules.
+#define BUFFERED_OUTPUT_FILTER_H
